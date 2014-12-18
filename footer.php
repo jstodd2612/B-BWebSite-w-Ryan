b616bc8e1688ffaf5d64b93538b0aa73b8510522
Jared Stoddard <jstodd2612@gmail.com>
2014-12-18 02:38:50 -0700

    CSS changes throughout, added PHP files for include statements

---
+++ footer.php
@@ -0,0 +1,51 @@
+<div class="row row-box-shadow" id="footer">
+        <div class="row">
+            <div class="large-4 medium-4 small-8 small-centered columns">
+                <a href="#"><img src="img/checkavail.png" /></a>
+                <a href="#"><img src="img/bb_dotcom.png" /></a>
+                <a href="#"><img src="img/tripAdvisor.png" /></a>
+            </div>
+            <div class="large-4 medium-4 small-8 small-centered columns">
+                <h5>Contact Us
+                    <span class="title-block"></span>
+                </h5>
+                <div>
+                Anne &amp; Nat Burkhardt - Innkeepers<br>
+                Susan &amp; Jeff Curtis - Innkeepers<br>
+                Sourwood Inn<br>
+                810 Elk Mountain Scenic Hwy.<br>
+                Asheville, North Carolina 28804 
+                </div>
+            </div>
+            <div class="large-4 medium-4 small-8 small-centered columns">
+                <h5>Contact Details
+                    <span class="title-block"></span>
+                </h5>
+                <ul class="">
+                    <li class="">(828) 255-0690</li>
+                    <li class="">810 Elk Mountain Scenic Hwy. Asheville, North Carolina 28804</li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="large-3 medium-5 small-8 large-centered medium-centered small-centered columns" id="socialIconsRow">
+                <ul class="socialMedia">
+                    <li><a target="_blank" href="#"><span id="facebook-icon"></span></a></li>
+                    <li><a target="_blank" href="#"><span id="twitter-icon"></span></a></li>
+                    <li><a target="_blank" href="#"><span id="pinterest-icon"></span></a></li>
+                    <li><a target="_blank" href="#"><span id="gplus-icon"></span></a></li>
+                </ul>
+            </div>  
+        </div>
+        <hr>
+        <div class="row">
+            <div class="large-12 columns">
+                <div class="large-3 medium-12 columns">
+                    <h6>UVU Student Final Project</h6>
+                </div>
+                <div class="large-3 medium-12 large-right columns">
+                    <h6>Contact / Check Availability</h6>
+                </div>
+            </div>
+        </div>
+    </div>
---
+++ header.php
@@ -0,0 +1,28 @@
+<header>
+    <div class="row">
+        <div class="large-12 columns" id="header">
+            <div class="large-5 medium-7 columns">
+                <div>
+                    <ul class="headerNav">
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Events</a></li>
+                        <li><a href="#">Gallery</a></li>
+                        <li><a href="#">About</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="large-12 columns" id="headerContact">
+            <div class="large-5 medium-7 columns">
+                <div>
+                    <ul class="headerNav">
+                        <li>info@abbingtongreen.com</li>
+                        <li><a href="tel://1-828-251-2454">(828) 251-2454</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
